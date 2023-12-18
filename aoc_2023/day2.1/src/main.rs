4c8068625329398336f91c9efd489d428021f085
Balamurugan R <emailstorbala@gmail.com>
2023-12-18 17:06:42 +0530

    AOC 2023: day1.2 completed. day2.1 in-progress

--- aoc_2023/day2.1/src/main.rs
+++ aoc_2023/day2.1/src/main.rs
@@ -1,8 +1,8 @@
 use clap::Parser;
+use sscanf;
+use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
-use std::collections::HashMap;
-use sscanf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -11,37 +11,39 @@ struct Args {
     file_name: String,
 }
 
-struct Bag {
+struct Game {
     red: i32,
     green: i32,
-    blue: i32
+    blue: i32,
 }
 
-fn read_contents(content: String) -> HashMap<i32, Bag> {
-    let mut game_details: HashMap<i32, Bag> = HashMap::new();
+fn read_contents(content: String) -> HashMap<i32, Game> {
+    let mut game_details: HashMap<i32, Game> = HashMap::new();
 
     for line in content.split('\n') {
-        if line.is_empty() { break };
+        if line.is_empty() {
+            break;
+        };
 
         // Sample -> Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
         let (game_id, game_data) = sscanf::sscanf!(line, "Game {i32}: {&str}").unwrap();
         // println!("game_id ->{game_id}<-");
         // println!("game_data->{game_data}<-");
 
-        let mut game = Bag {
+        let mut game = Game {
             red: 0,
             green: 0,
-            blue: 0
+            blue: 0,
         };
 
         for show in game_data.trim().split(';') {
             for data in show.trim().split(',') {
                 let (cnt, color) = sscanf::sscanf!(data.trim(), "{i32} {&str}").unwrap();
                 match color {
-                   "red" => game.red += cnt,
-                   "green" => game.green += cnt,
-                   "blue" => game.blue += cnt,
-                    _ => panic!("Invalid color detail provided!")
+                    "red" => game.red += cnt,
+                    "green" => game.green += cnt,
+                    "blue" => game.blue += cnt,
+                    _ => panic!("Invalid color detail provided!"),
                 };
             }
         }
@@ -52,7 +54,7 @@ fn read_contents(content: String) -> HashMap<i32, Bag> {
     return game_details;
 }
 
-fn get_game_total(game_details: HashMap<i32, Bag>) -> i32 {
+fn get_game_total(game_details: HashMap<i32, Game>) -> i32 {
     let mut game_total: i32 = 0;
     for game_id in game_details.keys() {
         let game = game_details.get(game_id).unwrap();
@@ -70,9 +72,9 @@ fn get_game_total(game_details: HashMap<i32, Bag>) -> i32 {
 fn main() {
     let args = Args::parse();
     let start_time = Instant::now();
-    let content = fs::read_to_string(&args.file_name).expect("Unable to load the file!");
+    let content:String = fs::read_to_string(&args.file_name).expect("Unable to load the file!");
 
-    let game_details = read_contents(content);
+    let game_details: HashMap<i32, Game> = read_contents(content);
     let game_total: i32 = get_game_total(game_details);
     println!("Game total is {game_total}");
 
