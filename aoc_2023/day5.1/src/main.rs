acb0d39108cc2f67401c0a2ee9481425f15b5345
Balamurugan R <emailstorbala@gmail.com>
2023-12-24 08:04:50 +0530

    AOC 2023: day5.1 improvised - 2. In-progress.

--- aoc_2023/day5.1/src/main.rs
+++ aoc_2023/day5.1/src/main.rs
@@ -15,16 +15,16 @@ struct Args {
 
 type CommonMap = HashMap<i64, i64>;
 
-struct Seeds {
-    seeds: Vec<i64>,
-}
-
-fn get_result_list(inp_list: Vec<i64>,
-                   src_list: Vec<i64>,
-                   dest_list: Vec<i64>,
-) -> Vec<i64> {
+fn get_result_list(inp_list: &Vec<i64>, line: &str) -> Vec<i64> {
     let mut tmp_list: Vec<i64> = Vec::new();
-    let mut common_map: CommonMap = HashMap::from([]);
+    let mut common_map: CommonMap = HashMap::new();
+
+    let (dest_range, src_range, range_len) = sscanf::sscanf!(line, "{i64} {i64} {i64}").unwrap();
+    println!("dest_range,src_range,range_len->{dest_range},{src_range},{range_len}");
+    let src_list: Vec<i64> = (src_range..src_range+range_len).collect();
+    println!("src_list prepared!");
+    let dest_list: Vec<i64> = (dest_range..dest_range+range_len).collect();
+    println!("dest_list prepared!");
 
     println!("Zipping started ...");
     for (src, dest) in zip(src_list, dest_list) {
@@ -33,13 +33,13 @@ fn get_result_list(inp_list: Vec<i64>,
     println!("Seed soil mode completed");
 
     // Collect soil list
-    for seed in inp_list {
-        let soil: i64 = if common_map.contains_key(&seed) {
-            *common_map.get(&seed).unwrap()
+    for src in inp_list {
+        let dest: i64 = if common_map.contains_key(&src) {
+            *common_map.get(&src).unwrap()
         } else {
-            seed
+            *src
         };
-        tmp_list.push(soil);
+        tmp_list.push(dest);
     }
 
     tmp_list
@@ -47,9 +47,7 @@ fn get_result_list(inp_list: Vec<i64>,
 
 
 fn read_contents(content: String) -> Vec<i64> {
-    let mut seeds = Seeds {
-        seeds: Vec::new(),
-    };
+    let mut seeds: Vec<i64> = Vec::new();
 
     let mut seed_to_soil_mode: bool = false;
     let mut soil_to_fert_mode: bool = false;
@@ -77,7 +75,7 @@ fn read_contents(content: String) -> Vec<i64> {
         println!("line is {line}");
         if line.starts_with("seeds:") {
             let seed_info = sscanf::sscanf!(line, "seeds: {&str}").unwrap();
-            seeds.seeds = seed_info
+            seeds = seed_info
                 .split_whitespace()
                 .map(|seed| -> i64 { seed.parse().unwrap() })
                 .collect();
@@ -93,34 +91,27 @@ fn read_contents(content: String) -> Vec<i64> {
                 "humidity-to-location map:" => humid_to_loc_mode = true,
                 _ => {
                     /* Do mode related operations */
-                    let (dest_range, src_range, range_len) = sscanf::sscanf!(line, "{i64} {i64} {i64}").unwrap();
-                    println!("dest_range,src_range,range_len->{dest_range},{src_range},{range_len}");
-                    let src_list: Vec<i64> = (src_range..src_range+range_len).collect();
-                    println!("src_list prepared!");
-                    let dest_list: Vec<i64> = (dest_range..dest_range+range_len).collect();
-                    println!("dest_list prepared!");
-
                     if seed_to_soil_mode {
                         println!("Seed->Soil mode started ...");
-                        tmp_list = get_result_list(seeds.seeds.clone(), src_list, dest_list);
+                        tmp_list = get_result_list(&seeds, line);
                     } else if soil_to_fert_mode {
                         println!("Soil->Fertilizer mode started ...");
-                        tmp_list = get_result_list(tmp_list.clone(), src_list, dest_list);
+                        tmp_list = get_result_list(&tmp_list, line);
                     } else if fert_to_water_mode {
                         println!("Fertilizer->Water mode started ...");
-                        tmp_list = get_result_list(tmp_list.clone(), src_list, dest_list);
+                        tmp_list = get_result_list(&tmp_list, line);
                     } else if water_to_light_mode {
                         println!("Water->Light mode started ...");
-                        tmp_list = get_result_list(tmp_list.clone(), src_list, dest_list);
+                        tmp_list = get_result_list(&tmp_list, line);
                     } else if light_to_temp_mode {
                         println!("Light->Temperature mode started ...");
-                        tmp_list = get_result_list(tmp_list.clone(), src_list, dest_list);
+                        tmp_list = get_result_list(&tmp_list, line);
                     } else if temp_to_humid_mode {
                         println!("Temperature->Humidity mode started ...");
-                        tmp_list = get_result_list(tmp_list.clone(), src_list, dest_list);
+                        tmp_list = get_result_list(&tmp_list, line);
                     } else if humid_to_loc_mode {
                         println!("Humidity->Location mode started ...");
-                        tmp_list = get_result_list(tmp_list.clone(), src_list, dest_list);
+                        tmp_list = get_result_list(&tmp_list, line);
                     } else {
                         panic!("Invalid mode detected!");
                     }
